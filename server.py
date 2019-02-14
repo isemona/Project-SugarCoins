51cf03f3d21cb51079361ca350912a9fd6198bba
Semona Igama <semona.igama@gmail.com>
2019-02-14 05:09:05 +0000

    Seeded user and gender tables

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, request, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+
