390c8752ab3f788f5f0e3741cd5c88f7392c0c45
Semona <semona.igama@gmail.com>
2019-08-30 11:19:12 -0700

    refactored/cleaned up code in send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -16,29 +16,6 @@ auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 my_number = os.environ.get('MY_NUMBER')
 twilio_number = os.environ.get('TWILIO_NUMBER')
 
-# def send_msg():
-#     user = get_user(session) # this changes everytime for each user so add it under the function
-#     fullname = user.name
-#     name_lst = fullname.split(' ')
-#     firstname = name_lst[0]
-#     balance = get_user_daily_balance(session) # this changes everytime for each user so add it under the function
-#     phone_number = get_phone_number(session) # this changes everytime for each user so add it under the function
-#     client = Client(account_sid, auth_token)
-#     print(f"client.messages \\"
-#           f"      .create("
-#           f"           body='Hi {user}, you have {balance} coins left in your SugarWallet. Spend wisely!'"
-#           f"           from_={twilio_number}"
-#           f"           to={phone_number}"
-#           f"       )")
-#     message = client.messages \
-#                     .create(
-#                          body=f"Hi {firstname}, you have {balance} coins left in your SugarWallet. Spend wisely!",
-#                          from_= twilio_number,
-#                          to= phone_number #my_number # phone_number variable here
-#                      )
-
-#     print(message.sid)
-
 def send_msg():
     users = User.query.filter(User.phone != None)
     for user in users:
@@ -96,6 +73,6 @@ if __name__ == "__main__":
 
     # continuous_thread = ScheduleThread()
     # continuous_thread.start()
-    print("Starting the Flask server")
+    # print("Starting the Flask server")
 
     # within a file two things are done: import the code and run it, if we're running this file as a program run __main__
